5803be28118387efbae14a8d58325ae11d01496c
Thomas Tanaka <thomas.tanaka@oracle.com>
2025-09-18 13:28:35 -0700

    Fix template
    Signed-off-by: Thomas Tanaka <thomas.tanaka@oracle.com>

---
+++ olm/go-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p bin
+version="{{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
+ldflags="
+        -s -w -extldflags '-static'
+        -extldflags=-L/usr/lib64
+        -X main.version=v${version}"
+
+go build --installsuffix cgo -trimpath=false -v -o ./bin/configmap-reload \
+	-ldflags "${ldflags}" configmap-reload.go
